11696e1dd1bfe35d721e0da0dab631a3d28a404e
Tony <anthony.teate@gmail.com>
2016-10-04 04:54:58 -0400

    added test function

---
+++ pycharm_test/test.py
@@ -0,0 +1,14 @@
+def add_two(x,y):
+    return(x+y)
+
+def multiply_two(x,y):
+    return x*y
+
+
+def main():
+    x=float(input("enter the first value "))
+    y=float(input("enter the second value "))
+    print("The sum of {0:.2f} and {1:.2f} is {2:.2f}".format(x, y, add_two(x,y)))
+
+if __name__ == "__main__":
+    main()
